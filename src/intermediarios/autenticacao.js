c73e1d8a2b1b9200c9882c990f6949eb5a67ff82
FrancineMBerto <francine.martim09@gmail.com>
2023-10-11 15:54:02 -0300

    adicionando midleware de autenticacao

---
+++ src/intermediarios/autenticacao.js
@@ -0,0 +1,38 @@
+const knex = require('../conexao')
+const jwt = require('jsonwebtoken')
+const senhaHash = require('../senhaHash')
+
+
+
+const verificarUsuarioLogado = async (req, res, next) => {
+   const { authorization } = req.headers
+
+   if (!authorization) {
+      return res.status(401).json({ mensagem: 'Não autorizado.' });
+   }
+
+
+
+   try {
+      const token = authorization.split(' ')[1];
+
+      const { id } = jwt.verify(token, senhaHash);
+
+      // const { rows, rowCount } = await pool.query(`select * from usuarios where id = $1`, [id])
+
+      const usuarioExiste = await knex('usuarios').where({ id }).first();
+
+      if (!usuarioExiste) {
+         return res.status(404).json({ mensagem: 'Token inválido.' });
+      }
+      const { senha, ...usuario } = UsuarioExiste
+
+      req.usuario = usuario;
+
+      next()
+   } catch (error) {
+      return res.status(401).json(error.message);
+   }
+}
+
+module.exports = verificarUsuarioLogado
