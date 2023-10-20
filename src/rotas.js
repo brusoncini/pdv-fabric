5b92f16b85e01217a2080dc2eaca2f2033a52cac
Bruna Soncini <brusoncini@gmail.com>
2023-10-20 20:47:25 -0300

    criação da rota deletar produto

--- src/rotas.js
+++ src/rotas.js
@@ -3,7 +3,7 @@ const verificarUsuarioLogado = require('./intermediarios/autenticacao')
 const { registrarUsuario, perfilUsuario, login, editarUsuario } = require("./controladores/usuarios");
 const { listarCategorias } = require("./controladores/categorias");
 const { registrarCliente, editarCliente } = require("./controladores/clientes")
-const { registrarProduto, listarProdutos, editarProduto } = require("./controladores/produtos");
+const { registrarProduto, listarProdutos, editarProduto, deletarProduto } = require("./controladores/produtos");
 
 const rotas = express();
 
@@ -21,11 +21,9 @@ rotas.put("/usuario", editarUsuario);
 rotas.post("/produto", registrarProduto);
 rotas.get("/produto", listarProdutos);
 rotas.put("/produto/:id", editarProduto);
-
+rotas.delete("/produto/:id", deletarProduto);
 
 rotas.post("/cliente", registrarCliente);
-rotas.put("/cliente/:id", editarCliente)
-
-
+rotas.put("/cliente/:id", editarCliente);
 
 module.exports = rotas;
