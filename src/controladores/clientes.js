377cbec693edee23e016a9f7fb1f11dc4405e5b1
FrancineMBerto <francine.martim09@gmail.com>
2023-10-20 00:17:40 -0300

    add rota de registrar cliente + integracão viacep

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -1,20 +1,21 @@
-const knex = require('knex');
-const axios = require('axios')
+const knex = require('../conexao');
+const enderecamento = require('../utilidades.js/enderecamento')
 
 
 const registrarCliente = async (req, res) => {
-   const { nome, email, cpf } = req.body
+   const { nome, email, cpf, cep, numero } = req.body
 
    if (!nome) {
       return res.status(404).json({ mensagem: 'O nome do cliente deve ser informado.' });
    }
-
    if (!email) {
       return res.status(404).json({ mensagem: 'O e-mail do cliente deve ser informado.' });
    }
-
    if (!cpf) {
       return res.status(404).json({ mensagem: 'O CPF do cliente deve ser informado.' });
+   }
+   if (cpf.length !== 11) {
+      return res.status(404).json({ mensagem: 'CPF inválido. Por favor, verifique se foi digitado corretamente' });
    }
 
    try {
@@ -27,20 +28,41 @@ const registrarCliente = async (req, res) => {
       const cpfExiste = await knex('clientes').where({ cpf }).first();
 
       if (cpfExiste) {
-         return res.status(404).json({ mensagem: 'O CPF informado já existe' });
+         return res.status(404).json({ mensagem: 'O CPF informado já existe.' });
+      }
+
+      let rua, bairro, cidade, estado;
+      if (cep) {
+         const dadosEndereco = await enderecamento.obterEndereco(cep)
+
+         if (!dadosEndereco || dadosEndereco.erro) {
+            return res.status(404).json({ mensagem: 'Endereço não encontrado. Tente novamente com um CEP válido.' });
+         }
+         // não obrigatório informar cep mas opcional.
+         rua = dadosEndereco.logradouro
+         bairro = dadosEndereco.bairro
+         cidade = dadosEndereco.localidade
+         estado = dadosEndereco.uf
       }
 
       const cliente = await knex('clientes').insert({
          nome,
          email,
-         cpf
+         cpf,
+         cep,
+         rua,
+         numero,
+         bairro,
+         cidade,
+         estado
       });
 
       if (!cliente) {
          return res.status(400).json({ mensagem: 'O cliente não foi cadastrado.' })
       }
 
-      return res.status(200).json({ mensagem: 'Cliente cadastrado com sucesso.' })
+      return res.status(200).json({ mensagem: "Cliente cadastrado com sucesso." })
+
    } catch (error) {
       return res.status(500).json(error.message)
    }
@@ -48,8 +70,5 @@ const registrarCliente = async (req, res) => {
 
 module.exports = {
    registrarCliente
+
 }
-
-// incompleto...
-
-//id nome email cpf cep rua numero bairro cidade estado //
