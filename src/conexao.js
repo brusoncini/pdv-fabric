e786dde4df0b9a3b3383199443f29bdbfad9a1b9
FrancineMBerto <francine.martim09@gmail.com>
2023-10-21 15:54:27 -0300

    add ssl

--- src/conexao.js
+++ src/conexao.js
@@ -5,7 +5,8 @@ const knex = require('knex')({
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        ssl: { rejectUnauthorized: false }
     }
 })
 
