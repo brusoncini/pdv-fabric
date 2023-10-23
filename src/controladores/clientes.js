ec2609f6415942dd81490652d868a8eb02ec3d04
Andressa Silva <silva.13579.2468@gmail.com>
2023-10-23 14:59:03 -0300

    Atualização de erro ortográfico

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -68,107 +68,105 @@ const registrarCliente = async (req, res) => {
 }
 
 
+
 const editarCliente = async (req, res) => {
-  const { id } = req.params
-  const { nome, email, cpf, cep, numero } = req.body
+  const { id } = req.params;
+  const { nome, email, cpf, cep, numero } = req.body;
 
   if (!nome) {
-    return res.status(400).json({ Mensagem: "O nome é obrigatório" })
+    return res.status(400).json({ Mensagem: "O nome é obrigatório" });
   }
   if (!email) {
-    return res.status(400).json({ Mensagem: "O e-mail é obrigatório" })
+    return res.status(400).json({ Mensagem: "O e-mail é obrigatório" });
   }
   if (!cpf) {
-    return res.status(400).json({ Mensagem: "O CPF é obrigatório" })
+    return res.status(400).json({ Mensagem: "O CPF é obrigatório" });
   }
 
   try {
-    const body = {}
-    let rua, bairro, cidade, estado
+    const body = {};
+    let rua, bairro, cidade, estado;
 
     if (nome) {
-      body.nome = nome
+      body.nome = nome;
     }
 
-
     if (email) {
-      const clienteExistente = await knex('clientes')
-        .where({ email }).whereNot({ id: id }).first()
+      const clienteExistente = await knex("clientes")
+        .where({ email })
+        .whereNot({ id: id })
+        .first();
 
       if (clienteExistente) {
-        return res.status(400).json("O email já está cadastrado para outro cliente")
+        return res
+          .status(400)
+          .json("O email já está cadastrado para outro cliente");
       }
       body.email = email;
     }
 
-
     if (cpf) {
       if (cpf.length !== 11) {
-        return res.status(400).json({ Mensagem: "CPF inválido" })
+        return res.status(400).json({ Mensagem: "CPF inválido" });
       }
-      const verificaCPF = await knex('clientes').where({ cpf }).count('id as count')
+      const verificaCPF = await knex("clientes")
+        .where({ cpf })
+        .count("id as count");
 
       if (verificaCPF.count > 0) {
-        return res.status(400).json({ Mensagem: "O CPF informado já está cadastrado" })
+        return res
+          .status(400)
+          .json({ Mensagem: "O CPF informado já está cadastrado" });
       }
-      body.cpf = cpf
+      body.cpf = cpf;
     }
-
 
     if (cep) {
-      const dadosEndereco = await enderecamento.obterEndereco(cep)
+      const dadosEndereco = await enderecamento.obterEndereco(cep);
 
       if (!dadosEndereco || dadosEndereco.erro) {
-        return res.status(404).json({ Mensagem: "Endereço não encontrado!" })
+        return res.status(404).json({ Mensagem: "Endereço não encontrado!" });
       }
-      rua = dadosEndereco.logradouro
-      bairro = dadosEndereco.bairro
-      cidade = dadosEndereco.localidade
-      estado = dadosEndereco.uf
+
+      rua = dadosEndereco.logradouro;
+      bairro = dadosEndereco.bairro;
+      cidade = dadosEndereco.localidade;
+      estado = dadosEndereco.uf;
     }
     if (rua) {
-      body.rua = rua
+      body.rua = rua;
     }
     if (numero) {
-      body.numero = numero
+      body.numero = numero;
     }
     if (bairro) {
-      body.bairro = bairro
+      body.bairro = bairro;
     }
     if (cidade) {
-      body.cidade = cidade
+      body.cidade = cidade;
     }
     if (estado) {
-      body.estado = estado
+      body.estado = estado;
     }
 
+    const clienteAtualizado = await knex("clientes")
+      .where({ id })
+      .update(body)
+      .returning("*");
 
-    const clienteAtualizado = await knex('clientes').where({ id }).update(body).returning('*')
-
-    console.log(body)
+    console.log(body);
 
     if (!clienteAtualizado) {
-      return res.status(400).json({ Mensagem: "Não foi possível atualizar o cliente" })
-    }
-
-    return res.status(200).json({ Mensagem: "Cliente atualizado com sucesso!", clienteAtualizado })
-
-  } catch (error) {
-    return res.status(400).json(error.message)
-  }
-}
-
-    if (!cliente) {
       return res
         .status(400)
-        .json({ mensagem: "O cliente não foi cadastrado." });
+        .json({ Mensagem: "Não foi possível atualizar o cliente" });
     }
 
     return res
       .status(200)
-      .json({ mensagem: "Cliente cadastrado com sucesso." });
+      .json({ Mensagem: "Cliente atualizado com sucesso!", clienteAtualizado });
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(400).json(error.message);
   }
 };
 
