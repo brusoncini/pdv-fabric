cd235c8f4c242f2d2fa72e2d476e15d98a83b7fd
FrancineMBerto <francine.martim09@gmail.com>
2023-10-26 19:27:27 -0300

    movendo rota de login do arquivo usuario pra outro

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,7 +1,6 @@
 const knex = require("../conexao");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const senhaHash = require("../senhaHash");
+
 
 const registrarUsuario = async (req, res) => {
   const { nome, email, senha } = req.body;
@@ -38,40 +37,6 @@ const registrarUsuario = async (req, res) => {
 
 const perfilUsuario = async (req, res) => {
   return res.status(200).json(req.usuario);
-};
-
-const login = async (req, res) => {
-  const { email, senha } = req.body;
-
-
-  try {
-
-    const usuario = await knex('usuarios').where({ email }).first();
-
-    if (!usuario) {
-      return res
-        .status(401)
-        .json({ mensagem: "E-mail e/ou senha inválido(s)." });
-    }
-
-    const senhaValida = await bcrypt.compare(senha, usuario.senha);
-
-    if (!senhaValida) {
-      return res
-        .status(401)
-        .json({ mensagem: "E-mail e/ou senha inválido(s)." });
-    }
-
-    const token = jwt.sign({ id: usuario.id }, senhaHash, {
-      expiresIn: "8h",
-    });
-
-    const { senha: _, ...usuarioLogado } = usuario;
-
-    return res.json({ usuario: usuarioLogado, token });
-  } catch (error) {
-    return res.status(500).json(error.message);
-  }
 };
 
 const editarUsuario = async (req, res) => {
@@ -119,6 +84,5 @@ const editarUsuario = async (req, res) => {
 module.exports = {
   registrarUsuario,
   perfilUsuario,
-  login,
   editarUsuario,
 };
