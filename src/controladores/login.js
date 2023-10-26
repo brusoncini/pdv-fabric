735b4ce4eaeea2c3325c0dbd0a665b60c73c9ca9
FrancineMBerto <francine.martim09@gmail.com>
2023-10-26 19:26:36 -0300

    movendo funcao login para arquivo login.js

---
+++ src/controladores/login.js
@@ -0,0 +1,41 @@
+const knex = require('../conexao')
+const jwt = require("jsonwebtoken");
+const senhaHash = require("../senhaHash");
+
+const login = async (req, res) => {
+   const { email, senha } = req.body;
+
+
+   try {
+
+      const usuario = await knex('usuarios').where({ email }).first();
+
+      if (!usuario) {
+         return res
+            .status(401)
+            .json({ mensagem: "E-mail e/ou senha inválido(s)." });
+      }
+
+      const senhaValida = await bcrypt.compare(senha, usuario.senha);
+
+      if (!senhaValida) {
+         return res
+            .status(401)
+            .json({ mensagem: "E-mail e/ou senha inválido(s)." });
+      }
+
+      const token = jwt.sign({ id: usuario.id }, senhaHash, {
+         expiresIn: "8h",
+      });
+
+      const { senha: _, ...usuarioLogado } = usuario;
+
+      return res.json({ usuario: usuarioLogado, token });
+   } catch (error) {
+      return res.status(500).json(error.message);
+   }
+};
+
+module.exports = {
+   login
+}
